f1d37fdcffb8281b6876dbac7305cfea26b0212c
amisha-13 <amishajoshi2713@gmail.com>
2025-12-21 13:37:33 +0530

    Initial Spring Boot application

---
+++ README.md
@@ -0,0 +1,5 @@
+String boot app:
+- Created using Spring Initializr
+- Exposed /endpoint
+- Exposed Actuator -/actuator/health
+- Used later for K8s liveness proble
---
+++ src/main/java/com/devops/springboot_app/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.devops.springboot_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Spring Boot App running 🚀";
+    }
+}
