b47470b0fac303898efadfcca91aafee5d83fcbd
amisha-13 <amishajoshi2713@gmail.com>
2026-01-04 15:54:48 +0530

    Modified pom.xml

--- src/main/java/com/devops/springboot_app/controller/HelloController.java
+++ src/main/java/com/devops/springboot_app/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.devops.springboot_app.controller;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
+
+    // Logger is defined at class level
+    private static final Logger log =
+            LoggerFactory.getLogger(HelloController.class);
+
+
+    @GetMapping("/hello")
     public String hello() {
-        return "Spring Boot App running 🚀";
+        log.info("User request received at /hello endpoint");
+        return "Hello from Spring Boot on EKS";
     }
 }
